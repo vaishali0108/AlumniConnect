371a1b4d945c1ffbc484ea554c7aaae8441c7dbf
Vaishali Choudhary <choudharyvaishali393@gmail.com>
2025-05-04 23:35:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://alumniconnectbackend.onrender.com', 
-        changeOrigin: true,
-      }
-    }
-  }
-})
 
+// No need to import tailwind like this, just use postcss config
+export default defineConfig({
+  plugins: [react()],
+})
